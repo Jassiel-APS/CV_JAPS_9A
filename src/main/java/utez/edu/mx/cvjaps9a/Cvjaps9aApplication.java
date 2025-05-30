1056d2c2aac1f2eff25a5050544d8958ca3d2ed6
Jassiel-APS <20223tn073@utez.edu.mx>
2025-05-29 23:32:52 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvjaps9a/Cvjaps9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjaps9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvjaps9aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cvjaps9aApplication.class, args);
+	}
+
+}
---
+++ src/main/java/utez/edu/mx/cvjaps9a/ServletInitializer.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvjaps9a;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cvjaps9aApplication.class);
+	}
+
+}
